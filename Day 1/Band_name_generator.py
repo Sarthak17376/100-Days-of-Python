413940ddb8b62c1a451df2420aecd2137f721414
Sarthak17376 <sarthak.dey.xyz@gmail.com>
2022-02-20 18:48:51 +0530

    Day1: Band-name-Generator

---
+++ Day 1/Band_name_generator.py
@@ -0,0 +1,7 @@
+''' Hi, I am Sarthak Dey, an young Python Developer. This program will help you come up with 
+a name for your local rock band which you never know might go on to be world famous one day'''
+
+user_city = input("Enter the city you grew up in:\n")
+user_pet = input("Enter your pet's name:\n")
+
+print("The name of your band is: " + user_city + " " + user_pet)
