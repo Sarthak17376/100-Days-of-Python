f1c59efed7f2524170aa9acd571ee7ff16bfa3a3
Sarthak Dey <sarthak.dey.xyz@gmail.com>
2023-07-09 00:02:49 +0530

    Create calculator.py

---
+++ Day 010/calculator.py
@@ -0,0 +1,55 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def calculator():
+    print(logo)
+
+    def add(n1, n2):
+        return n1 + n2
+
+    def sub(n1, n2):
+        return n1 - n2
+
+    def multiply(n1, n2):
+        return n1 * n2
+
+    def divide(n1, n2):
+        return n1 / n2
+
+    operations = {
+        "+": add,
+        "-": sub,
+        "*": multiply,
+        "/": divide
+    }
+
+    while input("Do you want to start a new calculation?(Y/N): ") == "Y":
+        num1 = float(input("Input the Number 1:\n"))
+        for symbols in operations:
+            print(symbols)
+        answer = num1
+        operation_symbol = input("Pick an operation from the symbols above\n")
+        num2 = float(input("Input the next number:\n"))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(answer, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    print("Calculator Shut Down!!!!")
+
+
+calculator()
