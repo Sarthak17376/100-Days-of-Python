99a289ab8c6e596212c5dbeeed2958ff7750a143
Sarthak Dey <sarthak.dey.xyz@gmail.com>
2023-10-07 01:44:04 +0530

    Create Database.py

---
+++ Day 015/Database.py
@@ -0,0 +1,32 @@
+menu = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "milk": 0,
+            "coffee": 18,
+        },
+        "cost": 180,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 250,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 350,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
